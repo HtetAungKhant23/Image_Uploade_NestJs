a36f0d08968dc2bfe50981181f10d2a5d275ed9b
htetaungkhant <htetaung23.ha@gmail.com>
2023-07-20 09:53:42 +0630

    array of file uploading complete by using FilesInterceptor and UploadedFiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MulterModule } from "@nestjs/platform-express";
 
 @Module({
-  imports: [
-    
-  ],
+  imports: [MulterModule.register({ dest: "./uploads" })],
   controllers: [AppController],
   providers: [AppService],
 })
